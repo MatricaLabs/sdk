37f7d0cbd1d717066ebb59d4b3bad20489686586
lilan <29991916+100xlongx@users.noreply.github.com>
2024-10-30 14:03:34 -0400

    spacing

--- src/errors.ts
+++ src/errors.ts
@@ -1,20 +1,20 @@
 export class MatricaOAuthError extends Error {
-  constructor(message: string, public code?: string) {
-    super(message);
-    this.name = 'MatricaOAuthError';
-    Object.setPrototypeOf(this, MatricaOAuthError.prototype);
-  }
+    constructor(message: string, public code?: string) {
+        super(message);
+        this.name = 'MatricaOAuthError';
+        Object.setPrototypeOf(this, MatricaOAuthError.prototype);
+    }
 }
 
 export class MatricaAuthenticationError extends MatricaOAuthError {
-  constructor(message: string) {
-    super(message, 'AUTHENTICATION_ERROR');
-    this.name = 'MatricaAuthenticationError';
-    Object.setPrototypeOf(this, MatricaAuthenticationError.prototype);
-  }
+    constructor(message: string) {
+        super(message, 'AUTHENTICATION_ERROR');
+        this.name = 'MatricaAuthenticationError';
+        Object.setPrototypeOf(this, MatricaAuthenticationError.prototype);
+    }
 }
 
 export default {
-  MatricaOAuthError,
-  MatricaAuthenticationError
+    MatricaOAuthError,
+    MatricaAuthenticationError
 }; 
--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -1,27 +1,27 @@
 export enum NetworkSymbol {
-  SOL = 'SOL',
-  ETH = 'ETH',
-  BTC = 'BTC',
-  MATIC = 'MATIC'
+    SOL = 'SOL',
+    ETH = 'ETH',
+    BTC = 'BTC',
+    MATIC = 'MATIC'
 }
 
 export enum WalletStatus {
-  HEALTHY = 'HEALTHY',
-  UNHEALTHY = 'UNHEALTHY'
+    HEALTHY = 'HEALTHY',
+    UNHEALTHY = 'UNHEALTHY'
 }
 
 export interface UserWallet {
-  id: string;
-  index: number;
-  networkSymbol: NetworkSymbol;
-  createdDate: string;
-  updatedDate: string;
-  status: WalletStatus;
-  isSearchSynced: boolean;
+    id: string;
+    index: number;
+    networkSymbol: NetworkSymbol;
+    createdDate: string;
+    updatedDate: string;
+    status: WalletStatus;
+    isSearchSynced: boolean;
 }
 
 export interface WalletToken {
-  walletId: string;
-  tokenId: string | null;
-  amount: number;
+    walletId: string;
+    tokenId: string | null;
+    amount: number;
 } 
